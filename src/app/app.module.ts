676b6dd2bd89dc477dca2906895b7ac57960113f
tossyyukky <snowsphere.burned@gmail.com>
2017-04-19 00:19:14 +0900

    Tutorial 7. HTTP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,34 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+import { HttpModule }    from '@angular/http';
 
 import { AppComponent }        from './app.component';
 import { HeroesComponent }     from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { DashboardComponent } from './dashboard.component';
+import { HeroSearchComponent } from './hero-search.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeroService } from './hero.service';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
   imports:      [
     BrowserModule,
     FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
     AppRoutingModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
   ],
   declarations: [
     AppComponent,
     DashboardComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroSearchComponent,
   ],
   providers: [ HeroService ],
   bootstrap:    [ AppComponent ]
